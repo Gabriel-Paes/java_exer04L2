3e9e84b241630396163c7e78572e5f93ff8c65ee
Gabriel-Paes <paessouzagabriel@gmail.com>
2022-10-07 21:45:45 -0300

    Function PositiveOrNegative

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.print("Digite um número: ");
+
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+
+        sc.close();
+
+        String retorno = verificaPositivoOuNegativo(n);
+
+        System.out.println("O número "+ n +" é um número "+retorno);
+    }
+    public static String verificaPositivoOuNegativo(int n){
+        if (n <= 0) {
+            return "N";
+        } else {
+            return "P";
+        }
     }
 }
